e40deee3a2ed4a28d27d735c2d0099f0a13161da
Khalil19-99 <67523643+Khalil19-99@users.noreply.github.com>
2020-08-28 18:14:58 +0300

    Update get.py

--- get.py
+++ get.py
@@ -1,12 +1,12 @@
 import requests
 import xlsxwriter
-
-workbook = xlsxwriter.Workbook('midsemester evaluation.xlsx')
+token = input("Enter your token: ")
+item = input("Enter your gradeitem: ")
+workbook = xlsxwriter.Workbook(str(item)+".xlsx")
 
 # The workbook object is then used to add new
 # worksheet via the add_worksheet() method.
 worksheet = workbook.add_worksheet()
-token = input("Enter your token: ")
 worksheet.write('A1', 'first name')
 worksheet.write('B1', 'last name')
 worksheet.write('C1', 'email')
@@ -18,20 +18,30 @@ for o in range(3):
     worksheet.write('A' + str(o + 2), site.json()[o]["firstname"])
     worksheet.write('B' + str(o + 2), site.json()[o]["lastname"])
     worksheet.write('C' + str(o + 2), site.json()[o]["email"])
-for j in range(1,3):
+for j in range(2,4):
+    grades = (
+            'http://10.90.105.115/moodle/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=gradereport_user_get_grades_table&wstoken=d297cefc19e8ad71923fe5223a6a8164&courseid=' + str(
+        j))
+
+    itemid=requests.get(grades)
+    for id in range(1,+1000):
+        s = itemid.json()["tables"][0]["tabledata"][id]["itemname"]["content"]
+        if s.find(item) != -1:
+            break
     sitee = (
                 'http://10.90.105.115/moodle/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=core_course_get_courses&wstoken=%s') % token
     site = requests.get(sitee)
-    worksheet.write(chr(67+j)+'1', site.json()[j]["fullname"])
+    worksheet.write(chr(66+j)+'1', site.json()[j-1]["fullname"])
     sitee = (
-       'http://10.90.105.115/moodle/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=gradereport_user_get_grade_items&wstoken=%s&courseid=2')%token
+       'http://10.90.105.115/moodle/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=gradereport_user_get_grade_items&wstoken=%s&courseid='+str(j))%token
     site=requests.get(sitee)
     # Use the worksheet object to write
     # data via the write() method.
     for i in range(3):
-        worksheet.write(chr(67+j) + str(i + 2), site.json()["usergrades"][i]["gradeitems"][3]["gradeformatted"])
+        worksheet.write(chr(67+j) + str(i + 2), site.json()["usergrades"][i]["gradeitems"][id-1]["gradeformatted"])
 
     # Finally, close the Excel file
     # via the close() method.
 workbook.close()
 #admin's token:efcc57f3b9deca9c98686e583d089e09
+#midsemester evaluation
