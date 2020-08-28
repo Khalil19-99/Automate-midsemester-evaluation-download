fdcd200f00589352816e80151282703a96ca550a
Khalil19-99 <67523643+Khalil19-99@users.noreply.github.com>
2020-08-28 20:38:21 +0300

    Update get.py

--- get.py
+++ get.py
@@ -3,6 +3,7 @@ import xlsxwriter
 token = input("Enter your token: ")
 item = input("Enter your gradeitem: ")
 workbook = xlsxwriter.Workbook(str(item)+".xlsx")
+item=item.lower()
 
 # The workbook object is then used to add new
 # worksheet via the add_worksheet() method.
@@ -20,12 +21,13 @@ for o in range(3):
     worksheet.write('C' + str(o + 2), site.json()[o]["email"])
 for j in range(2,4):
     grades = (
-            'http://10.90.105.115/moodle/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=gradereport_user_get_grades_table&wstoken=d297cefc19e8ad71923fe5223a6a8164&courseid=' + str(
-        j))
+            'http://10.90.105.115/moodle/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=gradereport_user_get_grades_table&wstoken=%s&courseid=' + str(
+        j))%token
 
     itemid=requests.get(grades)
     for id in range(1,+1000):
         s = itemid.json()["tables"][0]["tabledata"][id]["itemname"]["content"]
+        s=s.lower()
         if s.find(item) != -1:
             break
     sitee = (
@@ -38,10 +40,10 @@ for j in range(2,4):
     # Use the worksheet object to write
     # data via the write() method.
     for i in range(3):
-        worksheet.write(chr(67+j) + str(i + 2), site.json()["usergrades"][i]["gradeitems"][id-1]["gradeformatted"])
+        worksheet.write(chr(66+j) + str(i + 2), site.json()["usergrades"][i]["gradeitems"][id-1]["gradeformatted"])
 
     # Finally, close the Excel file
     # via the close() method.
 workbook.close()
 #admin's token:efcc57f3b9deca9c98686e583d089e09
-#midsemester evaluation
+
