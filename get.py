1d317f7c43df9b1b595e37b71b371b7ffc42f10a
Khalil19-99 <67523643+Khalil19-99@users.noreply.github.com>
2020-08-29 23:21:48 +0300

    Update get.py

--- get.py
+++ get.py
@@ -1,7 +1,10 @@
 import requests
 import xlsxwriter
+domain= input("Enter the moodle domain: ")
 token = input("Enter your token: ")
 item = input("Enter your gradeitem: ")
+if domain=="moodle.innopolis.university":
+    domain="http://10.90.105.115/moodle"
 workbook = xlsxwriter.Workbook(str(item)+".xlsx")
 item=item.lower()
 
@@ -14,15 +17,15 @@ worksheet.write('C1', 'email')
 
 for o in range(3):
     sitee = (
-       'http://10.90.105.115/moodle/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=core_enrol_get_enrolled_users&wstoken=%s&courseid=2')%token
+       '%s/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=core_enrol_get_enrolled_users&wstoken=%s&courseid=2')%(domain,token)
     site=requests.get(sitee)
     worksheet.write('A' + str(o + 2), site.json()[o]["firstname"])
     worksheet.write('B' + str(o + 2), site.json()[o]["lastname"])
     worksheet.write('C' + str(o + 2), site.json()[o]["email"])
 for j in range(2,4):
     grades = (
-            'http://10.90.105.115/moodle/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=gradereport_user_get_grades_table&wstoken=%s&courseid=' + str(
-        j))%token
+            '%s/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=gradereport_user_get_grades_table&wstoken=%s&courseid=' + str(
+        j))%(domain,token)
 
     itemid=requests.get(grades)
     for id in range(1,+1000):
@@ -31,11 +34,11 @@ for j in range(2,4):
         if s.find(item) != -1:
             break
     sitee = (
-                'http://10.90.105.115/moodle/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=core_course_get_courses&wstoken=%s') % token
+                '%s/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=core_course_get_courses&wstoken=%s') %(domain,token)
     site = requests.get(sitee)
     worksheet.write(chr(66+j)+'1', site.json()[j-1]["fullname"])
     sitee = (
-       'http://10.90.105.115/moodle/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=gradereport_user_get_grade_items&wstoken=%s&courseid='+str(j))%token
+       '%s/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=gradereport_user_get_grade_items&wstoken=%s&courseid='+str(j))%(domain,token)
     site=requests.get(sitee)
     # Use the worksheet object to write
     # data via the write() method.
@@ -46,4 +49,5 @@ for j in range(2,4):
     # via the close() method.
 workbook.close()
 #admin's token:efcc57f3b9deca9c98686e583d089e09
+#moodle.innopolis.university
 
